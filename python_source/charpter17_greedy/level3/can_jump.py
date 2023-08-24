1a3ebcc8d65758dd6991c24638dfeda40648989d
liuqingchao@fenbi.com <liuqingchao@fenbi.com>
2023-08-24 23:41:26 +0800

    位运算 python

--- python_source/charpter17_greedy/level3/can_jump.py
+++ python_source/charpter17_greedy/level3/can_jump.py
@@ -1,25 +1,17 @@
 # coding=utf-8
 
-class Candy:
-    def candy(self, ratings):
-        n = len(ratings)
-        left = [0] * n
+class CanJump:
+   def canJump(self, nums) :
+        n, rightmost = len(nums), 0
         for i in range(n):
-            if i > 0 and ratings[i] > ratings[i - 1]:
-                left[i] = left[i - 1] + 1
-            else:
-                left[i] = 1
-        right = ret = 0
-        for i in range(n - 1, -1, -1):
-            if i < n - 1 and ratings[i] > ratings[i + 1]:
-                right += 1
-            else:
-                right = 1
-            ret += max(left[i], right)
-        return ret
+            if i <= rightmost:
+                rightmost = max(rightmost, i + nums[i])
+                if rightmost >= n - 1:
+                    return True
+        return False
 
 
 if __name__ == '__main__':
     ratings = [1, 0, 2]
-    candy = Candy()
-    print candy.candy(ratings)
+    canJump = CanJump()
+    print canJump.canJump(ratings)
